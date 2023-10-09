f4cb6e6e73a8414e6c03dca6fdf2eb660e4754ec
Nickolas Daniel dos Santos Bento <nickolas_daniel@hotmail.com>
2023-10-09 16:23:41 -0300

    ajuste na tabela de conteudo

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CoursesComponent
+  ],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
+
   ]
 })
 export class CoursesModule { }
